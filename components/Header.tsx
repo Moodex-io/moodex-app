c6f2c4d02d7f88c31e40f9e6e35fe67a17fea947
Moodex Labs <moodexhelp@proton.me>
2025-09-02 11:16:08 -0400

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="brand">MOODEX LABS</div>
+      <span className="badge">BETA</span>
+    </header>
+  );
+}
